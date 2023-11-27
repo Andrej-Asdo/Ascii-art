2176a0fa6c4aaed1108c6627884575e079ba81c3
Andrej Kapusta <andrejkapusta5@gmail.com>
2023-11-27 12:41:57 +0100

    Edit the calculation of Linear Table

--- src/main/scala/Converter/TransformationTable/LinearTable/LinearTable.scala
+++ src/main/scala/Converter/TransformationTable/LinearTable/LinearTable.scala
@@ -3,5 +3,5 @@ package Converter.TransformationTable.LinearTable
 import Converter.TransformationTable.TransformationTable
 
 class LinearTable(table: String) extends TransformationTable {
-  def getCharacter(index: Int): Char = table.charAt(index % table.length)
+  def getCharacter(index: Int): Char = (table.charAt(((table.length) * index / 256).intValue()))
 }
